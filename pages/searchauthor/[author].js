91df27bd20de87e0550ebcb7ffd34352faa2fb87
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-12-14 15:09:13 -0300

    filtro
    filtro de pesquisa por titúlo de livro
    
    Co-Authored-By: João Vitor Gomes Dantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>

--- pages/searchauthor/[author].js
+++ pages/searchauthor/[author].js
@@ -1,15 +1,27 @@
 import useSWR from 'swr';
 import { useRouter } from 'next/router';
-import { Card, Table, Space, Button} from 'antd';
-import React, { useState } from 'react';
+import { Card, Table, Space, Button, Input} from 'antd';
+import React, { useRef, useState } from 'react';
 import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
 import "antd/dist/antd.css";
 
 export default function AuthorSearch(){
     const router = useRouter();
     const {author} = router.query;
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+      };
+      const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+      };
     const {data, error} = useSWR(`http://openlibrary.org/search.json?author=${author}`, fetcher);
-    
     if (error) return <div>falha na requisição...</div>
     if (!data) return <div>carregando...</div>
     if (data.Response=="False")return<div>autor não encontrado...</div>
@@ -31,7 +43,101 @@ export default function AuthorSearch(){
     });
 
     console.log(data2)
-
+    
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+          <div
+            style={{
+              padding: 8,
+            }}
+            onKeyDown={(e) => e.stopPropagation()}
+          >
+            <Input
+              ref={searchInput}
+              placeholder={`Search ${dataIndex}`}
+              value={selectedKeys[0]}
+              onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+              onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+              style={{
+                marginBottom: 8,
+                display: 'block',
+              }}
+            />
+            <Space>
+              <Button
+                type="primary"
+                onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                icon={<SearchOutlined />}
+                size="small"
+                style={{
+                  width: 90,
+                }}
+              >
+                Search
+              </Button>
+              <Button
+                onClick={() => clearFilters && handleReset(clearFilters)}
+                size="small"
+                style={{
+                  width: 90,
+                }}
+              >
+                Reset
+              </Button>
+              <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                  confirm({
+                    closeDropdown: false,
+                  });
+                  setSearchText(selectedKeys[0]);
+                  setSearchedColumn(dataIndex);
+                }}
+              >
+                Filter
+              </Button>
+              <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                  close();
+                }}
+              >
+                close
+              </Button>
+            </Space>
+          </div>
+        ),
+        filterIcon: (filtered) => (
+          <SearchOutlined
+            style={{
+              color: filtered ? '#1890ff' : undefined,
+            }}
+          />
+        ),
+        onFilter: (value, record) =>
+          record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+          if (visible) {
+            setTimeout(() => searchInput.current?.select(), 100);
+          }
+        },
+        render: (text) =>
+          searchedColumn === dataIndex ? (
+            <Highlighter
+              highlightStyle={{
+                backgroundColor: '#ffc069',
+                padding: 0,
+              }}
+              searchWords={[searchText]}
+              autoEscape
+              textToHighlight={text ? text.toString() : ''}
+            />
+          ) : (
+            text
+          ),
+      });
     
     const columns = [
         {
@@ -42,6 +148,7 @@ export default function AuthorSearch(){
             
             sorter: (a, b) => a.title.localeCompare(b.title),
             sortDirections: ['descend', 'ascend'],
+            ...getColumnSearchProps('title'),
         },
         {
             title: 'First_publish_year',
